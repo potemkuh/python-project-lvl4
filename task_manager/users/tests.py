a0504dd9216f54a055746c20fbae92cb45d173ab
grins <potemkuh@gmail.com>
2021-12-13 19:35:18 +0300

    test

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+
+class TestRegistrAndLogin(TestCase):
+    def test_register(self):
+        client = Client()
+        username = 'test_user'
+        password1 = '123Rjhjdfve'
+        password2 = '123Rjhjdfve'
+        user = {
+            'first_name': 'test_first',
+            'last_name': 'test_last',
+            'username': username,
+            'password1': password1,
+            'password2': password2 
+        }
+        reg = client.post(reverse('create'), user, follow=True)
+
+        self.assertEqual(reg.status_code, 200)
