76d1562b80bda83cd46184aaf19241737e6d30fe
grins <potemkuh@gmail.com>
2021-12-15 21:20:31 +0300

    fix url users

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -27,7 +27,7 @@ class CreateUser(SuccessMessageMixin, CreateView):
     successmessage = 'User successfully registered'
 
     def get_success_url(self):
-        return reverse('login')
+        return reverse('users')
 
 
 class LoginView(LoginView):
