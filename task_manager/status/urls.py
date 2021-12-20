d93078b2ff562746d342953449d910b8aa0a1714
grins <potemkuh@gmail.com>
2021-12-20 20:12:08 +0300

    fix urls statuslist

--- task_manager/status/urls.py
+++ task_manager/status/urls.py
@@ -3,7 +3,7 @@ from task_manager.status import views
 
 
 urlpatterns = [
-    path('statuses/', views.StatusList.as_view(), name='statuses'),
+    path('', views.StatusList.as_view(), name='statuses'),
     path('create/', views.StatusCreate.as_view(), name='create_status'),
     path('<int:pk>/update/', views.StatusEdit.as_view(), name='status_edit'),
     path('<int:pk>/delete/', views.StatusDelete.as_view(), name='status_delete'),
