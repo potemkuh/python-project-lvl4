9db2c3126ac6498fc0f938af3a343f96fb3adfac
grins <potemkuh@gmail.com>
2021-12-20 20:08:56 +0300

    fix urls status

--- task_manager/status/urls.py
+++ task_manager/status/urls.py
@@ -4,7 +4,7 @@ from task_manager.status import views
 
 urlpatterns = [
     path('statuses/', views.StatusList.as_view(), name='statuses'),
-    path('statuses/create/', views.StatusCreate.as_view(), name='create_status'),
-    path('statuses/<int:pk>/update/', views.StatusEdit.as_view(), name='status_edit'),
-    path('statuses/<int:pk>/delete/', views.StatusDelete.as_view(), name='status_delete'),
+    path('create/', views.StatusCreate.as_view(), name='create_status'),
+    path('<int:pk>/update/', views.StatusEdit.as_view(), name='status_edit'),
+    path('<int:pk>/delete/', views.StatusDelete.as_view(), name='status_delete'),
 ]
