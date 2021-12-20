82f0535855b78d840ab6b80cf678f2ae1eb3262c
grins <potemkuh@gmail.com>
2021-12-20 19:38:09 +0300

    add alert base

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView
-from django.urls import reverse
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-class LoginView(LoginView):
+class LoginView(SuccessMessageMixin, LoginView):
     template_name = 'users/login.html'
+
+
+    success_message = 'Вы залогинены'
+
