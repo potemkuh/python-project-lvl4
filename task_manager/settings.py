41d0d91c1c8300544d591a98883828164debb39c
grins <potemkuh@gmail.com>
2022-01-04 14:45:38 +0300

    translate

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -113,16 +113,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_L10N = False
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- task_manager/task/forms.py
+++ task_manager/task/forms.py
@@ -1,9 +1,10 @@
 from django.forms import ModelForm
 from task_manager.task.views import Task
+from django.utils.translation import gettext_lazy as _
 
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields =['name', 'description', 'status', 'executor', 'labels']
+        fields =['name', _('description'), _('status'), _('executor'), _('labels')]
