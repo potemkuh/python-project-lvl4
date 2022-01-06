96e01a79c7f1f9fd19674cb72afd957c6011602a
grins <potemkuh@gmail.com>
2022-01-06 14:51:07 +0300

    fix def __str__

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     
-    def str(self):
+    def __str__(self):
         return self.get_full_name()
 
 class Status(models.Model):
