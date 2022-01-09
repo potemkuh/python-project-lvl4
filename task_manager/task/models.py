6114005723f89f6178c621356efed75d9f17418d
grins <potemkuh@gmail.com>
2022-01-09 16:44:39 +0300

    fix

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -8,13 +8,34 @@ from task_manager.label.models import Label
 
 class Task(models.Model):
     name = models.CharField(_('name'), max_length=64)
-    author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='author', verbose_name=_('author'))
-    executor = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='executor', verbose_name=_('executor'), blank=True, null=True)
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='status', verbose_name=_('status'))
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='author',
+        verbose_name=_('author')
+        )
+    executor = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='executor',
+        verbose_name=_('executor'),
+        blank=True, null=True
+        )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name='status',
+        verbose_name=_('status')
+        )
     description = TextField(_('description'), blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(blank=True, null=True)
-    labels = models.ManyToManyField(Label, related_name='label', verbose_name=_('labels'), blank=True)
+    labels = models.ManyToManyField(
+        Label,
+        related_name='label',
+        verbose_name=_('labels'),
+        blank=True
+        )
 
     def __str__(self):
         return self.name
