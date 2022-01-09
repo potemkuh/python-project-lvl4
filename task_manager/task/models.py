c486f4495f835f84adbfa7918197429997ec8272
grins <potemkuh@gmail.com>
2022-01-09 16:53:53 +0300

    fix e123 lint2

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -12,21 +12,18 @@ class Task(models.Model):
         get_user_model(),
         on_delete=models.PROTECT,
         related_name='author',
-        verbose_name=_('author')
-        )
+        verbose_name=_('author'))
     executor = models.ForeignKey(
         get_user_model(),
         on_delete=models.PROTECT,
         related_name='executor',
         verbose_name=_('executor'),
-        blank=True, null=True
-        )
+        blank=True, null=True)
     status = models.ForeignKey(
         Status,
         on_delete=models.PROTECT,
         related_name='status',
-        verbose_name=_('status')
-        )
+        verbose_name=_('status'))
     description = TextField(_('description'), blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(blank=True, null=True)
@@ -34,8 +31,7 @@ class Task(models.Model):
         Label,
         related_name='label',
         verbose_name=_('labels'),
-        blank=True
-        )
+        blank=True)
 
     def __str__(self):
         return self.name
